663acb8f395adbe7efd425ff4c271bf50ff2bd0b
kaye wu <apiswswsw@gmail.com>
2015-01-10 20:42:50 -0800

    send pubnub

--- index.js
+++ index.js
@@ -26,13 +26,41 @@ app.get('/flag', function(request, response) {
 app.get('/flagset', function(request, response) { 
 	flag = true;
 	response.send(flag);
+	// send pubnub, this will cause phone and watch to blinking red
+	var message = {"alert": "alert"};
+	sendPubnub(message);
 });
 
 app.get('/flagclear', function(request, response) {
 	flag = false;
 	response.send(flag);
+	// send pubnub, phone and watch will not blink red
+	var message = {"fine": "fine"};
+	sendPubnub(message);
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+
+function sendPubnub(message) {
+
+	var pubnub = require("pubnub")({
+	    ssl           : true,  // <- enable TLS Tunneling over TCP
+	    publish_key   : "demo",
+	    subscribe_key : "demo"
+	});
+
+	/* ---------------------------------------------------------------------------
+	Publish Messages
+	--------------------------------------------------------------------------- */
+	//var message = { "alert" : "alert" };
+	pubnub.publish({ 
+	    channel   : 'bmwwatchalert',
+	    message   : message,
+	    callback  : function(e) { console.log( "SUCCESS!", e ); },
+	    error     : function(e) { console.log( "FAILED! RETRY PUBLISH!", e ); }
+	});
+
+}
